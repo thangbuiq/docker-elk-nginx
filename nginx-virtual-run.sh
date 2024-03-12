696046a2cbc6c27fbe3efed1f07ac5119a944c99
thangqbui-kms <thangqbui@kms-technology.com>
2024-03-12 13:34:31 +0700

    update

--- nginx-virtual-run.sh
+++ nginx-virtual-run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Set the URL of your NGINX server
 NGINX_URL="http://localhost"
@@ -10,16 +10,16 @@ NUM_REQUESTS=456
 
 # Function to make requests to NGINX
 make_requests() {
-    for ((i=0; i<$NUM_REQUESTS; i++)); do
-        # Select a random route
-        ROUTE="$(printf "%s\n" ${ROUTE_LIST[@]} | shuf | head -1)"
-        PATH=${NGINX_URL}${ROUTE}
-        echo "Making request to $PATH"
-        curl -s -o /dev/null $PATH
-          # Delay a random amount of time between requests
-        # DELAY=$(awk -v min=0 -v max=0.2 'BEGIN{srand(); print min+rand()*(max-min)}')
-        # sleep $DELAY
-    done
+  for ((i=0; i<$NUM_REQUESTS; i++)); do
+    # Select a random route
+    ROUTE="$(printf "%s\n" ${ROUTE_LIST[@]} | shuf | head -1)"
+    PATH=${NGINX_URL}${ROUTE}
+    echo "Making request to $PATH"
+    curl -s -o /dev/null $PATH
+      # Delay a random amount of time between requests
+    # DELAY=$(awk -v min=0 -v max=0.2 'BEGIN{srand(); print min+rand()*(max-min)}')
+    # sleep $DELAY
+  done
 }
 
 # Make requests
