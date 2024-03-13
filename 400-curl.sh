b531eaea1673f6801e5693b1a632f4f3612eeec0
thangqbui-kms <thangqbui@kms-technology.com>
2024-03-13 21:27:27 +0700

    feat: response code rework

---
+++ 400-curl.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# Function to make requests to NGINX
+make_requests() {
+  for ((i=0; i<100; i++)); do
+    # Select a random route
+    curl http://localhost/error/
+      # Delay a random amount of time between requests
+    # DELAY=$(awk -v min=0 -v max=0.2 'BEGIN{srand(); print min+rand()*(max-min)}')
+    # sleep $DELAY
+  done
+}
+
+# Make requests
+make_requests
